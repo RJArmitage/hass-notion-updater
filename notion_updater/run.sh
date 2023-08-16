514cedc1afeba9cad7b45dffa9e0bd23b7079430
Richard Armitage <rarmitag@tibco.com>
2023-08-16 20:26:54 +0100

    Testing initial setup

--- notion_updater/run.sh
+++ notion_updater/run.sh
@@ -4,6 +4,12 @@ set -e
 # CONFIG_PATH=/data/options.json
 # CONNECTION_STRING="$(jq --raw-output '.connectionString' $CONFIG_PATH)"
 
+NOTION_TOKEN=$(bashio::config 'access_token')
+JOURNAL_DB_ID=$(bashio::config 'journal_db_id')
+
+echo "Token: ${NOTION_TOKEN}"
+echo "Journal ID: ${JOURNAL_DB_ID}"
+
 echo Hello!
 node -v
 npm -v
